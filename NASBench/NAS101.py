d7791b70672175b404ca3d1c97abbea7cc70e9de
ngctnnnn <ngctnnnn@gmail.com>
2022-02-22 13:39:16 +0700

    fix bug

--- NASBench/NAS101.py
+++ NASBench/NAS101.py
@@ -131,7 +131,7 @@ class NAS101(NASBench):
             return ops, ops_none
         
         ops, ops_none = get_operations(ind)
-        ind = ind[5:]
+        ind = convert_ind_triangle(ind[5:], ops_none)
         
         self.cell = api.ModelSpec(convert_ind_triangle(ind[5:], ops_none), ops)
          
