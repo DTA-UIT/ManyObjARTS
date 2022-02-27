b01215ad809e5cfc0764a94ce992751bec7adb8d
ngctnnnn <ngctnnnn@gmail.com>
2022-02-27 10:58:49 +0700

    fix bug

--- source/nasbench1shot1/optimizers/regularized_evolution/run_regularized_evolution.py
+++ source/nasbench1shot1/optimizers/regularized_evolution/run_regularized_evolution.py
@@ -27,7 +27,7 @@ from source.nasbench1shot1.nasbench_analysis.search_spaces.search_space_1 import
 from source.nasbench1shot1.nasbench_analysis.search_spaces.search_space_2 import SearchSpace2
 from source.nasbench1shot1.nasbench_analysis.search_spaces.search_space_3 import SearchSpace3
 from source.nasbench1shot1.nasbench_analysis.utils import upscale_to_nasbench_format, INPUT, OUTPUT, CONV1X1, CONV3X3, MAXPOOL3X3
-from source.nasbench.nasbench import api101
+from source.nasbench.nasbench import api as api101
 
 def train_and_eval(config):
     adjacency_matrix, node_list = config.adjacency_matrix, config.node_list
